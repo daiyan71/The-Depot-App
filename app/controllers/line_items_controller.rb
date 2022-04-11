7f0ce92678ea5fd40256feced525832c81482c81
daiyan71 <daiyanibrahim12@gmail.com>
2022-04-11 11:45:06 +0600

    finished till cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
@@ -24,12 +24,14 @@ before_action :set_cart, only: [:create]
   # POST /line_items or /line_items.json
   def create
     # @line_item = LineItem.new(line_item_params)
+    session[:counter] = nil
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    #@line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -69,6 +71,6 @@ before_action :set_cart, only: [:create]
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,5 +4,11 @@ class StoreController < ApplicationController
     @time = DateTime.now.strftime('%I:%M %p')
     @date = DateTime.now.strftime('%d:%m %y')
     #@products = Product.all
+    if session[:counter].nil?
+      session[:counter]=1
+    else
+      session[:counter]+=1
+    end
+    @cnt = session[:counter]
   end
 end
